287acff9c95725d2438845aae9c39fa0d511b79e
justinpakzad-sb <justin.pakzad@wearesilverbullet.com>
2024-08-28 14:54:03 -0400

    Update deploy.py

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -2,7 +2,7 @@ import os
 import tdworkflow
 
 
-def deploy_td_project(region, project_folder):
+def deploy_td_project(instance_type, project_folder,api_key):
     endpoint = os.environ[f"SYSTEM_TD_WORKFLOW_ENDPOINT_{region.upper()}"]
     api_key = os.environ[f"TD_API_KEY_{region.upper()}"]
     client = tdworkflow.client.Client(api_key=api_key, endpoint=endpoint)
@@ -11,6 +11,11 @@ def deploy_td_project(region, project_folder):
 
 
 if __name__ == "__main__":
-    region = os.environ.get("REGION", "EU")
-    project_path = os.path.join(os.getcwd(), region.lower())
-    deploy_td_project(region, project_path)
+    instance_type = os.environ.get("INSTANCE", "DEV")
+    api_key_eu = os.environ[f"TD_API_KEY_EU_{instance_type.upper()}"]
+    api_key_us = os.environ[f"TD_API_KEY_US_{instance_type.upper()}"]
+    project_path_eu = os.path.join(os.getcwd(), "eu")
+    project_path_us = os.path.join(os.getcwd(), "us")
+    deploy_td_project(project_path_eu,endpoint_eu,api_key_eu)
+    deploy_td_project(project_path_us,endpoint_us,api_key_us)
+    # deploy_td_project(region, project_path)
