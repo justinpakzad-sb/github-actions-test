303863fbdb53d066b9a326cc412e8f81486c375a
justin-pakzad_marsjustinpakzad-sb <justin.pakzad@effem.com>
2024-08-28 15:30:41 -0400

    initial commit

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -2,20 +2,46 @@ import os
 import tdworkflow
 
 
-def deploy_td_project(instance_type, project_folder,api_key):
-    endpoint = os.environ[f"SYSTEM_TD_WORKFLOW_ENDPOINT_{region.upper()}"]
-    api_key = os.environ[f"TD_API_KEY_{region.upper()}"]
+def get_api_keys(instance_type: str) -> str:
+    """Returns corresponding API keys depending on instance type (dev/prod)"""
+    api_key_eu = os.environ[f"TD_API_KEY_EU_{instance_type.upper()}"]
+    api_key_us = os.environ[f"TD_API_KEY_US_{instance_type.upper()}"]
+    return api_key_eu, api_key_us
+
+
+def get_project_paths():
+    """Returns project paths for eu/us regions"""
+    project_path_eu = os.path.join(os.getcwd(), "eu")
+    project_path_us = os.path.join(os.getcwd(), "us")
+    return project_path_eu, project_path_us
+
+
+def get_endpoints():
+    """Returns endpoints for eu/us regions"""
+    us_endpoint = "https://console-next.treasuredata.com"
+    eu_endpoint = "https://console-next.eu01.treasuredata.com"
+    return us_endpoint, eu_endpoint
+
+
+def deploy_td_project(project_folder, api_key, endpoint):
+    """Deploys projects to TD via workflow API client"""
     client = tdworkflow.client.Client(api_key=api_key, endpoint=endpoint)
     os.chdir(project_folder)
-    client.create_project(region, ".")
+    client.create_project(project_folder, ".")
+
+
+def main():
+    instance_type = os.environ.get("INSTANCE", "DEV")
+    api_key_eu, api_key_us = get_api_keys(instance_type)
+    project_path_eu, project_path_us = get_project_paths()
+    eu_endpoint, us_endpoint = get_endpoints()
+    deploy_td_project(
+        project_folder=project_path_eu, api_key=api_key_eu, endpoint=eu_endpoint
+    )
+    deploy_td_project(
+        project_folder=project_path_us, api_key=api_key_us, endpoint=us_endpoint
+    )
 
 
 if __name__ == "__main__":
-    instance_type = os.environ.get("INSTANCE", "DEV")
-    api_key_eu = os.environ[f"TD_API_KEY_EU_{instance_type.upper()}"]
-    api_key_us = os.environ[f"TD_API_KEY_US_{instance_type.upper()}"]
-    project_path_eu = os.path.join(os.getcwd(), "eu")
-    project_path_us = os.path.join(os.getcwd(), "us")
-    deploy_td_project(project_path_eu,endpoint_eu,api_key_eu)
-    deploy_td_project(project_path_us,endpoint_us,api_key_us)
-    # deploy_td_project(region, project_path)
+    main()
